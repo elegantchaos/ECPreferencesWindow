f3c4f8f66cd80f469d7f6a7317144dcec12592d2
Sam Deane <sam@elegantchaos.com>
2013-02-05 13:03:55 +0000

    Improved docs

--- Source/Mac/ECPWController.h
+++ Source/Mac/ECPWController.h
@@ -18,7 +18,7 @@
  The class isn't a singleton, although typically you'll simply make one instance
  and store it somewhere (eg in your application delegate).
 
- Call <showPreferencesWindow> to show the preferences window.
+ Call showPreferencesWindow to show the preferences window.
 
 	ECPWController* controller = [ECPWController preferencesWindowController];
 	[controller showPreferencesWindow];
@@ -27,7 +27,7 @@
  # Initialisation And Configuration
 
  Most configuration is done via the ECPreferencesWindow key of the application's
- info.plist file.
+ Info.plist file.
  
  This key should contain a dictionary.
  
@@ -44,30 +44,42 @@
  describes a pane to add to the preferences window.
 
  Each pane dictionary can contain the following keys:
+
  - Class (required): the class to use for the pane.
  - Identifier: the identifier to use for the pane. Defaults to the class name.
  - Name: the text label to use for the pane. Defaults to the class name.
  - Icon: the name of the icon image to use for the panel. Defaults to the class name.
  - ToolTip: tooltip text to use for the pane. Defaults to nil.
 
+
 # Preference Bundles
  
- In addition to loading the panes described in main application's info.plist, the
+ In addition to loading the panes described in main application's Info.plist, the
  controller also scans a folder for preference bundles, and loads any that it finds.
  
- Each of these bundles is then asked to provide a list of pane dictionaries for additional
- panes to load.
+ The principal class for each of these bundles should inherit from ECPWBundle.
+
+ When a preferences bundle is loaded, [ECPWBundle preferencesController:loadedBundle:] is called on this class.
+
+ This method should return a list of dictionaries with additional panes to load.
  
- # Timing
+ The default implementation of the method does this by looking in the bundle's Info.plist file
+ for an ECPreferencesWindow key, and taking a list of panes from the Panes sub-key (this mirrors
+ the process used to load panes from the main application's Info.plist).
+
+ # Loading
 
  The bundles folder is scanned when the controller is first created, and any preference
- bundles that are found get called at this point in order to set themselves up.
+ bundles that are found get loaded and called at this point in order to set themselves up.
  
  This allows preferences bundles to add features to an application when it first starts up,
  rather than only doing something when the preferences window is shown.
  
  The pane classes themselves are created lazily when the preferences window is first shown,
  so it's quite possible that they will never be created at all for a given run of an application.
+ 
+
+ When a pane is loaded, [ECPWPane paneDidLoad] is called on it.
 
  */
 

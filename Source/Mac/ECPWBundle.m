26af159f0fc4850afe6ce6b269e3143a8bcc68d7
Sam Deane <sam@elegantchaos.com>
2014-04-11 18:06:54 +0100

    Updated copyright date.

--- Source/Mac/ECPWBundle.m
+++ Source/Mac/ECPWBundle.m
@@ -3,7 +3,7 @@
 //  ECPreferencesWindow
 //
 //  Created by Sam Deane on 04/02/2013.
-//  Copyright (c) 2013 Elegant Chaos. All rights reserved.
+//  Copyright 2014 Sam Deane, Elegant Chaos. All rights reserved.
 //
 
 #import "ECPWBundle.h"
